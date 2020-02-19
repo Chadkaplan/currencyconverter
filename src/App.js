dcbd2ad0c1b39cdc59b7afcd010fb39ac2cb4a31
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-02-19 16:13:45 -0500

    Added on change event to handle the amount of each currency. Works while changing either value to calculate for the other value.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
   const [fromCurrency, setFromCurrency] = useState()
   const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
 
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount= amount
+    toAmount = amount * exchangeRate
+  }
+  else{
+    toAmount= amount
+    fromAmount = amount / exchangeRate
+  }
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
@@ -17,8 +29,19 @@ function App() {
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
         setFromCurrency(data.base)
         setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
       })
   }, [])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <>
@@ -26,14 +49,17 @@ function App() {
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency= {fromCurrency}
-        onChangeCurrency= {event => setFromCurrency(event.target.value)}
-
+        onChangeCurrency= {e => setFromCurrency(e.target.value)}
+        onChangeAmount = {handleFromAmountChange}
+        amount = {fromAmount}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency= {toCurrency}
-        onChangeCurrency= {event => setToCurrency(event.target.value)}
+        onChangeCurrency= {e => setToCurrency(e.target.value)}
+        onChangeAmount = {handleToAmountChange}
+        amount = {toAmount}
       />
     </>
   );
